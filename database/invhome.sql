7572ac54f96704aa2f093a57c8b625457292b51c
danishiqbaldishu <danish00iqbal786@gmail.com>
2022-11-18 14:10:36 +0530

    update mysql database seeds

--- database/invhome.sql
+++ database/invhome.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2022 at 02:49 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Nov 18, 2022 at 09:38 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -60,15 +60,6 @@ CREATE TABLE `posts` (
   `post_created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`post_id`, `post_title`, `post_slug_url`, `post_description`, `post_category`, `post_thumbnail`, `post_meta_tags`, `post_meta_description`, `post_created_at`) VALUES
-(18, 'first post', 'first-post', '<p>this is just for testing</p>\r\n', '7', 'http://localhost:8080/upload/modern-banner-important-announcement-business-260nw-1784320871.jpg', 'just forb test', 'meta description', '2022-10-23 00:34:00'),
-(20, 'third post about chair', 'third-post-about-chair', '<p>chair</p>\r\n', '8', 'http://localhost:8080/upload/bg3.jpg', 'meta tags,', 'djfgjgrfj', '2022-10-23 00:42:15'),
-(21, 'fourth set', 'fourth-set', '<p>sjdvj</p>\r\n', '7', 'http://localhost:8080/upload/products/grocery_thumbnail.jpg', 'iki', 'jji', '2022-10-23 00:45:23');
-
 -- --------------------------------------------------------
 
 --
@@ -88,8 +79,16 @@ CREATE TABLE `post_category` (
 --
 
 INSERT INTO `post_category` (`post_category_id`, `post_category_title`, `post_category_slug`, `category_thumbnail`, `createt`) VALUES
-(7, 'sofa set', 'sofa-set', 'http://localhost:8080/upload/categories/sofa.png', '2022-10-23 00:17:04'),
-(8, 'chair', 'chair', 'http://localhost:8080/upload/categories/chair-1.png', '2022-10-22 23:53:14');
+(7, 'SOFA SET', 'sofa-set', 'http://localhost:8080/upload/categories/sofa.png', '2022-10-29 14:42:24'),
+(8, 'CHAIR', 'chair', 'http://localhost:8080/upload/categories/chair-1.png', '2022-10-29 14:43:01'),
+(9, 'BED', 'bed', 'http://localhost:8080/upload/categories/bed.png', '2022-10-29 14:42:43'),
+(10, 'CONSOLE', 'console', 'http://localhost:8080/upload/categories/console.png', '2022-10-29 14:43:11'),
+(11, 'DINING TABLE', 'dining-table', 'http://localhost:8080/upload/categories/dining.png', '2022-10-29 14:43:30'),
+(12, 'END TABLE', 'end-table', 'http://localhost:8080/upload/categories/endtable1.png', '2022-10-29 14:40:25'),
+(13, 'MEDIA UNIT', 'media-unit', 'http://localhost:8080/upload/categories/unit.png', '2022-10-29 14:40:58'),
+(14, 'SIDE TABLE', 'side-table', 'http://localhost:8080/upload/categories/sidetable.png', '2022-10-29 14:41:53'),
+(15, 'COMMERCIAL FURNITURE', 'commercial-furniture', 'http://localhost:8080/upload/categories/vector-office.png', '2022-11-01 03:48:19'),
+(16, 'CENTER TABLE', 'center-table', 'http://localhost:8080/upload/categories/tavble.jpg', '2022-11-01 06:06:40');
 
 --
 -- Indexes for dumped tables
@@ -127,13 +126,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `post_category`
 --
 ALTER TABLE `post_category`
-  MODIFY `post_category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `post_category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
